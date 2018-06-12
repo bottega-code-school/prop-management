fec120ebe733fb0479158cd2378a796e4c82140b
Max <maxnelson997@gmail.com>
2018-06-12 17:39:14 -0600

    built newsletterbox and functionality

---
+++ src/components/newsletter/newsletterBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+
+export default function NewsletterBox({date}) {
+    return (
+        <div className='newsletter-box'>
+            <div className='newsletter-box__day'>{date.getDate()}</div>
+            <div className='newsletter-box__month-year'>{months[date.getMonth()]} {date.getFullYear()}</div>
+        </div>
+    )
+}
--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import NewsletterBox from './newsletterBox';
+
 class NewsletterGrid extends Component {
     render() {
         return (
             <div className='newsletter-grid'>
-asdfasdfasdf
+                {/* add button */}
+                <NewsletterBox date={new Date()}/>
+                {/* archive */}
+                {/* content */}
             </div>
         )
     }
