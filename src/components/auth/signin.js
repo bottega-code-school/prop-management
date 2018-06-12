81d41e39ea6bba995d4b50fb14c68a6ae31a739e
Max <maxnelson997@gmail.com>
2018-06-11 18:52:36 -0600

    built signup form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -6,7 +6,6 @@ class Signin extends Component {
     render() {
         return (
             <div className='sign-in'>
-                
                 <SigninForm/>
             </div>
         )
