0c8a892d60fdba4c6046865f90b1f6f40dc5e08d
Max <maxnelson997@gmail.com>
2018-06-11 20:23:27 -0600

    handled signup post request

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -6,12 +6,13 @@ import { ROOT_URL } from '../config';
 export function signUp(fields, success) {
     console.log(fields);
     return function(dispatch) {
-        // axios.post(`${ROOT_URL}/signUp`, fields)
-        //     .then(response => {
-        //         //do something with response
-        //     })
-        //     .catch(err => {
-        //         if(err) { console.log(err) }
-        //     })
+        axios.post(`${ROOT_URL}/signUp`, fields)
+            .then(response => {
+               console.log(response);
+               success()
+            })
+            .catch(err => {
+                if(err) { console.log(err) }
+            })
     }
 }
