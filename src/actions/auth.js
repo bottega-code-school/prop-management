ca03ed64aa93a17c9e3b88d2253667964c8e1c83
Max <maxnelson997@gmail.com>
2018-06-11 20:48:11 -0600

    AUTHENTICATE USER reducer

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -3,13 +3,19 @@ import axios from 'axios';
 
 import { ROOT_URL } from '../config';
 
+import {
+    AUTHENTICATE_USER
+} from './types';
+
 export function signUp(fields, success) {
-    console.log(fields);
     return function(dispatch) {
         axios.post(`${ROOT_URL}/signUp`, fields)
             .then(response => {
-               console.log(response);
-               success()
+               dispatch({
+                   type: AUTHENTICATE_USER,
+                   payload: response.data
+               })
+               success();
             })
             .catch(err => {
                 if(err) { console.log(err) }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
 import { reducer as form } from 'redux-form';
+import auth from './authReducer';
+
 
 const rootReducer = combineReducers({
-  form
-
+  form,
+  auth
 });
 
 export default rootReducer;
