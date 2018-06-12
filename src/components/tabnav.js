ba3bd52ce15695ba323e00570f675304f2e25b3c
Max <maxnelson997@gmail.com>
2018-06-12 16:20:29 -0600

    added callback to tabnav to handle clicking a tab

--- src/components/tabnav.js
+++ src/components/tabnav.js
@@ -7,11 +7,23 @@ class TabNav extends Component {
             <div className='tab-nav__tabs'>
             {
                 this.props.tabs.map((tab, index) => {
-                    return <a className='tab-nav__tab'>{tab.title}</a>
+                    return <a key={index} onClick={() => this.props.handleClick(tab.title)} className='tab-nav__tab'>{tab.title}</a>
                 })
             }
             </div>
-            <div>requests or newsletters content goes here</div>
+            
+            {
+                this.props.tabs.map((tab, index) => {
+                    if(tab.active) {
+                        return (
+                            <div key={index} className='tab-nav__component'>
+                                {tab.component}
+                            </div>
+                        )
+                    }
+                })
+            }
+    
         </div>
     )
   }
