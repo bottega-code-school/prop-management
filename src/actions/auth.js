8aad760b001e1a3c27327834d4c5016ac72d2eb0
Max <maxnelson997@gmail.com>
2018-06-12 13:04:54 -0600

    set token in local storage

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -11,6 +11,8 @@ export function signUp(fields, success) {
     return function(dispatch) {
         axios.post(`${ROOT_URL}/signUp`, fields)
             .then(response => {
+                const { token } = response.data;
+                localStorage.setItem('token', token);
                dispatch({
                    type: AUTHENTICATE_USER,
                    payload: response.data
