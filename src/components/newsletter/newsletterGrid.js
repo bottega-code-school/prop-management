8a70e924fd0c4daf1918be514197f5d9f174fe0d
Max <maxnelson997@gmail.com>
2018-06-13 17:25:29 -0600

    hooked up latestnewsletter and datebox component to store to fetch data

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -14,25 +14,18 @@ class NewsletterGrid extends Component {
     }
 
     componentDidMount() {
-        this.props.fetchNewsletters();
+        setTimeout(() => {
+            this.props.fetchNewsletters();
+        }, 1000);
     }
 
     render() {
-
-        const latest = {
-            _id: '115',
-            title: 'Happy Holidays Fam',
-            body: 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
-            date: new Date(),
-            imageUrl: 'http://via.placeholder.com/960x258'
-        }
-
         return (
             <div className='newsletter-grid'>
                 <Button className='newsletter-grid__button' icon='fas fa-plus' callback={() => this.handleAddNewsletter()}/>
-                <NewsletterBox date={new Date()}/>
+                <NewsletterBox/>
                 <NewsletterArchive/>
-                <NewsletterLatest history={this.props.history} {...latest}/>
+                <NewsletterLatest history={this.props.history}/>
             </div>
         )
     }
