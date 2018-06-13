a0d0c57cf95a9a6138382d53c00a1cbc28fee933
Max <maxnelson997@gmail.com>
2018-06-13 11:39:11 -0600

    built newsletter new component and placed signinform into newsletter new component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -22,6 +22,7 @@ import Signin from './components/auth/signin';
 
 // DASHBOARD
 import Dashboard from './components/dashboard';
+import NewNewsletter from './components/newsletter/newsletterNew';
 
 function main() {
   ReactDOM.render(
@@ -34,6 +35,7 @@ function main() {
             <Route path='/signup' component={Signup}/>
 
             <Route path='/dashboard' component={requireAuth(Dashboard)}/>
+            <Route path='/newsletter/new' component={requireAuth(NewNewsletter)}/>
           </Layout>
         </Switch>
       </Router>
---
+++ src/components/newsletter/newsletterNew.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import SigninForm from '../auth/signinForm';
+
+class NewNewsletter extends Component {
+
+    onSubmit = (fields) => {
+        console.log('trying to handle submit')
+    }
+
+    render() {
+        return (
+            <div className='new-newsletter'>
+                <SigninForm onSubmit={(event) => this.onSubmit(event)}/>
+            </div>
+        )
+    }
+}
+
+export default NewNewsletter;
