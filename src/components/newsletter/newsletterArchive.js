869efc5e8e61079ecd22e25f59f6532c428de652
Max <maxnelson997@gmail.com>
2018-06-13 17:05:22 -0600

    hooked up newsletterarchive to redux store mapstatetoprops to display newsletters data in the archive

--- src/components/newsletter/newsletterArchive.js
+++ src/components/newsletter/newsletterArchive.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
 function ArchiveItem({title, date}) {
     return (
         <div className='archive-item archive-items__item'>  
@@ -22,14 +24,23 @@ class NewsletterArchive extends Component {
                 <div className='newsletter-archive__title'>Archive</div>
                 <div className='newsletter-archive__items archive-items'>
                     {/* newsletter items */}
-                    <ArchiveItem title='hey' date={new Date()}/>
-                    <ArchiveItem title='hey' date={new Date()}/>
-                    <ArchiveItem title='hey' date={new Date()}/>
-                    <ArchiveItem title='hey' date={new Date()}/>
+                    
+                    {
+                        this.props.newsletters.map(newsletter => {
+                            return <ArchiveItem key={newsletter._id} {...newsletter}/>
+                        })
+                    }
                 </div>
             </div>
         )
     }
 }
 
-export default NewsletterArchive;
+function mapStateToProps(state) {
+    const { newsletters } = state.newsletters;
+    return {
+        newsletters
+    }
+} 
+
+export default connect(mapStateToProps)(NewsletterArchive);
