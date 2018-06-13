4692a339f26cf354226eb014adf06ab2efe1e540
Max <maxnelson997@gmail.com>
2018-06-12 21:16:00 -0600

    created and placed add newsletter button on newsletter grid

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 import NewsletterBox from './newsletterBox';
 import NewsletterArchive from './newsletterArchive';
 import NewsletterLatest from './newsletterLatest';
+import Button from '../button';
 
 class NewsletterGrid extends Component {
+    
+    handleAddNewsletter = () => {
+        this.props.history.push('/newsletter/new');
+    }
+
     render() {
 
         const latest = {
@@ -17,7 +23,7 @@ class NewsletterGrid extends Component {
 
         return (
             <div className='newsletter-grid'>
-                {/* add button */}
+                <Button className='newsletter-grid__button' icon='fas fa-plus' callback={() => this.handleAddNewsletter()}/>
                 <NewsletterBox date={new Date()}/>
                 <NewsletterArchive/>
                 <NewsletterLatest {...latest}/>
