ec8e5c524142e0d9b6dac4b6003bab607c32cf4d
Max <maxnelson997@gmail.com>
2018-06-14 17:49:41 -0600

    built requests reducer and change selected request type action creator

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux';
 
 import { reducer as form } from 'redux-form';
+
 import auth from './authReducer';
 import newsletters from './newsletterReducer';
-
+import requests from './requestsReducer';
 
 const rootReducer = combineReducers({
   form,
   auth,
-  newsletters
+  newsletters,
+  requests
 });
 
 export default rootReducer;
