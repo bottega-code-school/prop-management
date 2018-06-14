8a44bda461ae1cd81ee0d24364116e5ab9aac5f0
Max <maxnelson997@gmail.com>
2018-06-14 15:39:55 -0600

    cleaned up newsletter detail grid

--- src/components/newsletter/newsletterDetail.js
+++ src/components/newsletter/newsletterDetail.js
@@ -6,6 +6,8 @@ import * as actions from '../../actions';
 import NewsletterBox from './newsletterBox';
 import NewsletterLatest from './newsletterLatest';
 
+import { FormTitle } from '../formTitle';
+
 class NewsletterDetail extends Component {
 
     componentDidMount() {
@@ -16,6 +18,7 @@ class NewsletterDetail extends Component {
         console.log(this.props.newsletterToEdit);
         return (
             <div className='newsletter-detail'>
+                <FormTitle className='newsletter-detail__title' text='Newsletter Detail'/>
                 <NewsletterBox date={this.props.newsletterToEdit.date}/>
                 <NewsletterLatest {...this.props.newsletterToEdit}/>
             </div>
