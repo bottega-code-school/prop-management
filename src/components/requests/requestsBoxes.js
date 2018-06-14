906f164f87d62ff15c2d3e89240665cba9846566
Max <maxnelson997@gmail.com>
2018-06-14 16:49:09 -0600

    built requests components and placed on grid

---
+++ src/components/requests/requestsBoxes.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class RequestsBoxes extends Component {
+    render() {
+        return (
+            <div className='requests-boxes'>
+                pending
+                inprogress
+                complete
+            </div>
+        )
+    }
+}
+
+export default RequestsBoxes;
