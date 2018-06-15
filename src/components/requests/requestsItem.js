0da5f1992c6115f3999464e90c3e6a378bfb14e1
Max <maxnelson997@gmail.com>
2018-06-15 11:17:43 -0600

    cleaned up request item grid

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -10,12 +10,12 @@ class RequestsItem extends Component {
             <div className='requests-item'>
                 <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>
                 <div className='requests-item__title'>
-                    Yo my door fell down
+                    <div className='requests-item__title__text'>Yo my door fell down</div>
+                    <Icon className='requests-item__title__arrow' icon='fas fa-sort-down'/>
                 </div>
                 <div className='requests-item__tenant-unit'>
                     Max - Unit 115
                 </div>
-                <Icon className='requests-item__arrow' icon='fas fa-sort-down'/>
                 <div className='requests-item__date'>
                     09/15/97
                 </div>
