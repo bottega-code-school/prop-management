4918b1663048b64aad5264f50b65dacd206510f4
Max <maxnelson997@gmail.com>
2018-06-15 16:20:59 -0600

    successfuly implemented new request feature

--- src/components/requests/requestsNew.js
+++ src/components/requests/requestsNew.js
@@ -16,7 +16,7 @@ class NewRequest extends Component {
     formData.append('body', body);
     formData.append('image', image);
 
-    this.props.createNewRequest(this.props._id, fields, () => {
+    this.props.createNewRequest(this.props._id, formData, () => {
         this.props.history.push("/dashboard");
     })
     
