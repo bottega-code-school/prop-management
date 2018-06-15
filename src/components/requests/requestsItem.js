453cccb153aefe8a9c88ade1c7a5887b1ae4f452
Max <maxnelson997@gmail.com>
2018-06-15 17:02:20 -0600

    mapped over item data to requestsitem component

--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -5,6 +5,8 @@ import Button from '../button';
 
 import AnimateHeight from 'react-animate-height';
 
+import { ROOT_URL } from '../../config';
+
 class RequestsItem extends Component {
     
     constructor() {
@@ -27,18 +29,24 @@ class RequestsItem extends Component {
     }
 
     render() {
+        const { _id, title, body, date, imageUrl, status } = this.props;
+        const parsedDate = new Date(date);
         return (
             <div id='requests-item' className='requests-item'>
                 <Icon className='requests-item__icon' icon='fas fa-exclamation-triangle'/>
                 <div className='requests-item__title'>
-                    <div className='requests-item__title__text'>Yo my door fell down</div>
+                    <div className='requests-item__title__text'>{title}</div>
                     <Icon callback={() => this.toggleDropdown()} className='requests-item__title__arrow' icon='fas fa-sort-down'/>
                 </div>
                 <div className='requests-item__tenant-unit'>
                     Max - Unit 115
                 </div>
                 <div className='requests-item__date'>
-                    09/15/97
+                    { parsedDate.getMonth() + 1 }
+                    /
+                    { parsedDate.getDate() }
+                    /
+                    { parsedDate.getFullYear() - 2000 } 
                 </div>
                 <Button className='requests-item__move' icon='fas fa-wrench' callback={() => console.log('tryna change request status')}/>
                 <div className='requests-item__description'>
@@ -49,13 +57,10 @@ class RequestsItem extends Component {
                         <div className='item-description'>
                             <img 
                                 className='item-description__img'
-                                src='http://via.placeholder.com/160x94'                        
+                                src={`${ROOT_URL}/${imageUrl}`}                        
                             />
                             <p className='item-description__text'>
-                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-                                Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+                                {body}
                             </p>
                         </div>
                     </AnimateHeight>
