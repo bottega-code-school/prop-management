d3943395d4a6e1b81b4a72595dd8e08d83a38395
Max <maxnelson997@gmail.com>
2018-06-15 16:47:45 -0600

    created type to set requests and reducer to handle said type.

--- src/components/requests/requests.js
+++ src/components/requests/requests.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
 import RequestsItem from './requestsItem';
 
 class Requests extends Component {
     render() {
         return (
             <div className='requests'>
-                <RequestsItem/>
-                <RequestsItem/>
-                <RequestsItem/>
+                {
+                    this.props.requests.map(requestItem => {
+                        return <RequestsItem {...requestItem} key={requestItem._id}/>
+                    })
+                }
             </div>
         )
     }
 }
 
-export default Requests;
+function mapStateToProps(state) {
+    const { requests } = state.requests;
+    return {
+        requests
+    }
+}
+
+export default connect(mapStateToProps)(Requests);
--- src/reducers/requestsReducer.js
+++ src/reducers/requestsReducer.js
@@ -1,5 +1,6 @@
 import {
-    CHANGE_SELECTED_REQUEST_TYPE
+    CHANGE_SELECTED_REQUEST_TYPE,
+    SET_REQUESTS
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -14,6 +15,11 @@ export default function(state = INITIAL_STATE, action) {
             return {
                 ...state,
                 selectedRequestType: boxType
+            }
+        case SET_REQUESTS: 
+            return {
+                ...state,
+                requests: action.payload
             }
         default: return state;
     }
