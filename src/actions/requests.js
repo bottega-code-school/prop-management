1730443b0ca6df14635bebf00c7200821be6831c
Max <maxnelson997@gmail.com>
2018-06-15 18:05:46 -0600

    implemented callback into changeStatus to re-fetch items upon change status completion

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -52,7 +52,7 @@ export function fetchRequests() {
     }
 }
 
-export function changeStatus({_id, status}) {
+export function changeStatus({_id, status}, success) {
     const token = localStorage.getItem('token');
     return function() {
         axios.post(`${ROOT_URL}/requests/update-status`, {_id, status}, {
@@ -60,6 +60,7 @@ export function changeStatus({_id, status}) {
         })
             .then(response => {
                 console.log(response.data);
+                success();
             })
             .catch(err => {
                 console.log(err);
--- src/components/requests/requestsItem.js
+++ src/components/requests/requestsItem.js
@@ -31,8 +31,15 @@ class RequestsItem extends Component {
         }
     }
 
+    handleStatus = () => {
+        const { _id, status } = this.props;
+        this.props.changeStatus({_id, status}, () => {
+            this.props.fetchRequests();
+        })
+    }
+
     render() {
-        const { _id, title, body, date, imageUrl, status } = this.props;
+        const { title, body, date, imageUrl } = this.props;
         const parsedDate = new Date(date);
         return (
             <div id='requests-item' className='requests-item'>
@@ -51,7 +58,7 @@ class RequestsItem extends Component {
                     /
                     { parsedDate.getFullYear() - 2000 } 
                 </div>
-                <Button className='requests-item__move' icon='fas fa-wrench' callback={() => this.props.changeStatus({_id, status})}/>
+                <Button className='requests-item__move' icon='fas fa-wrench' callback={() => this.handleStatus()}/>
                 <div className='requests-item__description'>
                     <AnimateHeight
                         duration={300}
