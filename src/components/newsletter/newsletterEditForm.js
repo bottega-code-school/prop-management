a9c0d0ec0b7590f3efd6e8daf1f85effc4bbda2c
Max <maxnelson997@gmail.com>
2018-06-16 12:04:09 -0600

    redux form reinitialize option

--- src/components/newsletter/newsletterEditForm.js
+++ src/components/newsletter/newsletterEditForm.js
@@ -69,7 +69,8 @@ class EditNewsletterForm extends Component {
 }
 
 EditNewsletterForm = reduxForm({
-  form: "editnewsletter"
+  form: "editnewsletter",
+  enableReinitialize: true
 })(EditNewsletterForm);
 
 function mapStateToProps(state) {
