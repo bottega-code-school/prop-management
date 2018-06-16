1ce7b3332164d1cd455cd95b1a8bf7bd565cf947
Max <maxnelson997@gmail.com>
2018-06-16 14:33:04 -0600

    built require admin component and used it on the new newsletter button

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -7,6 +7,8 @@ import NewsletterArchive from './newsletterArchive';
 import NewsletterLatest from './newsletterLatest';
 import Button from '../button';
 
+import RequireAdmin from '../auth/requireAdmin';
+
 class NewsletterGrid extends Component {
     
     handleAddNewsletter = () => {
@@ -22,7 +24,9 @@ class NewsletterGrid extends Component {
     render() {
         return (
             <div className='newsletter-grid'>
-                <Button className='newsletter-grid__button' icon='fas fa-plus' callback={() => this.handleAddNewsletter()}/>
+                <RequireAdmin>
+                    <Button className='newsletter-grid__button' icon='fas fa-plus' callback={() => this.handleAddNewsletter()}/>
+                </RequireAdmin>
                 <NewsletterBox {...this.props.latestNewsletter} />
                 <NewsletterArchive/>
                 <NewsletterLatest {...this.props.latestNewsletter}/>
